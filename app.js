82b3f9a0a25690c2c5332346baacea9e95b747d0
Shashi Kiran Deshetti <deshetti@Shashis-MacBook-Pro.local>
2019-03-28 13:33:50 -0500

    removed test data

--- app.js
+++ app.js
@@ -4,12 +4,6 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-const students = ["Elie", "Matt", "Joel", "Michael"];
-
-app.get("/", (req, res) => {
-    return res.json(students);
-});
-
 app.listen(() => {
     console.log("Server starting on port 3000");
 });
