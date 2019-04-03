ff8986745c21ef1af91fb6a7cafd65b96892bfed
Shashi Kiran Deshetti <deshetti@Shashis-MacBook-Pro.local>
2019-04-03 01:55:27 -0500

    changed the default values in factcheck.js

--- static/schema/factcheck.js
+++ static/schema/factcheck.js
@@ -3,19 +3,19 @@ const schema = {
     '@context': 'http://schema.org',
     '@type': 'ClaimReview',
     datePublished: '2014-07-23',
-    url: 'http://www.politifact.com/texas/statements/2014/jul/23/rick-perry/rick-perry-claim-about-3000-homicides-illegal-immi/',
+    url: 'http://factcheck.factly.in/factcheck/-fact-checking-government-claims-on-direct-benefit-transfer-0',
     author: {
         '@type': 'Organization',
         url: 'http://factcheck.factly.in/',
         image: 'http://gateway.factly.in/core/dega-content/factly-telugu/2019/4/factly-logo.png'
     },
-    claimReviewed: 'More than 3,000 homicides were committed by \'illegal aliens\' over the past six years.',
+    claimReviewed: '',
     reviewRating: {
         '@type': 'Rating',
         ratingValue: -1,
         bestRating: 5,
         worstRating: 1,
-        image: 'http://static.politifact.com.s3.amazonaws.com/rulings/tom-pantsonfire.gif',
+        image: '',
         alternateName: 'True'
     },
     itemReviewed: {
