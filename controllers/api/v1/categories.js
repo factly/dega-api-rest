288faaa00cd3383190605ab39ed3973c763ea4eb
Umang <umangshah97@gmail.com>
2019-06-12 01:23:12 +0530

    pagination for categories. This closes  #56

--- controllers/api/v1/categories.js
+++ controllers/api/v1/categories.js
@@ -6,7 +6,15 @@ function getCategory(req, res, next) {
     utils.setLogTokens(logger, 'factchecks', 'getFactcheck', req.query.client, null);
     const model = new CategoryModel(logger);
     const clientId = req.query.client;
-    return model.getCategory(req.app.kraken, clientId).then((result) => {
+    return model.getCategory(
+        req.app.kraken, 
+        clientId, 
+        req.query.sortBy,
+        req.query.sortAsc,
+        req.query.limit,
+        req.query.next,
+        req.query.previous
+    ).then((result) => {
         if (result) {
             res.status(200).json(result);
             return;
