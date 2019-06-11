7cf6e214f5eeabc59282f4fefae62a14b848c828
Umang <umangshah97@gmail.com>
2019-06-12 01:40:42 +0530

    Add pagination to formats. This closes #60

--- models/format.js
+++ models/format.js
@@ -1,6 +1,6 @@
-
 const MongoBase = require('../lib/MongoBase');
 const Q = require('q');
+const MongoPaging = require('mongo-cursor-pagination');
 
 class FormatModel extends MongoBase {
     /**
@@ -13,18 +13,28 @@ class FormatModel extends MongoBase {
         this.logger = logger;
     }
 
-    getFormat(config, clientId, slug) {
+    getFormat(config, clientId, slug, sortBy, sortAsc, limit, next, previous) {
         // get query object
         const query = this.getQueryObject(clientId, slug);
         if (clientId) {
             query.client_id = clientId;
         }
-
-        return Q(this.collection(config.get('databaseConfig:databases:core')).find(query).toArray())
-            .then((results) => {
+        const pagingObj = this.getPagingObject(query, sortBy, sortAsc, limit, next, previous);
+        const database = config.get('databaseConfig:databases:core');
+        return Q(MongoPaging.find(this.collection(database),pagingObj))
+            .then((result) => {
+                console.log(result.results)
                 this.logger.info('Retrieved the results');
-                return results;
-         });
+                result["data"] = result.results;
+                let response = {};
+                response["data"] = result.results;
+                response["paging"] = {};
+                response["paging"]["next"] = result.next;
+                response["paging"]["hasNext"] = result.hasNext;
+                response["paging"]["previous"] = result.previous;
+                response["paging"]["hasPrevious"] = result.hasPrevious;
+                return response
+            });
     }
 
     getQueryObject(clientId, slug) {
@@ -37,6 +47,28 @@ class FormatModel extends MongoBase {
             queryObj.slug = slug;
         }
         return queryObj;
+    }
+    getPagingObject(queryObj, sortBy, sortAsc, limit, next, previous) {
+        const pagingObj = {};
+        pagingObj.query = queryObj;
+        pagingObj.limit = (limit) ? parseInt(limit) : 20;
+
+        if (sortBy) {
+            pagingObj.paginatedField = sortBy;
+        }
+
+        if (sortAsc) {
+            pagingObj.sortAscending = (sortAsc === 'true');
+        }
+
+        if (next) {
+            pagingObj.next = next;
+        }
+
+        if (previous) {
+            pagingObj.previous = previous;
+        }
+        return pagingObj;
     }
 }
 
