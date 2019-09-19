20f172285b92b703bb98d994fef5f16185e8d3b3
Monark <monark@factly.in>
2019-09-19 14:37:27 +0530

    claimant aggregations added

--- models/claimant.js
+++ models/claimant.js
@@ -19,9 +19,52 @@ class ClaimantModel extends MongoBase {
         if (clientId) {
             query.client_id = clientId;
         }
-        const pagingObj = utils.getPagingObject(query, sortBy, sortAsc, limit, next, previous);
+        const match = { $match: query };
+
+        const aggregations = [
+            {
+                $project: {
+                    id: "$_id",
+                    _id: 0,
+                    class: "$_class",
+                    name: 1,
+                    tagLine: '$tag_line',
+                    description: 1,
+                    slug: 1,
+                    clientId: "$client_id",
+                    media: 1,
+                    createdDate: "$created_date",
+                    lastUpdatedDate: "$last_updated_date"
+                }
+            },
+            match
+        ]
+
+        const pagingObj = utils.getPagingObject(aggregations, sortBy, sortAsc, limit, next, previous, true);
         const database = config.get('databaseConfig:databases:factcheck');
         return Q(MongoPaging.find(this.collection(database), pagingObj))
+            .then((claimants) => {
+                const mediaAggregation = utils.mediaPipeline;
+
+                let mediaIds = claimants.results.filter(claimant => claimant.media).map(claimant => claimant.media.oid)
+                
+                const match = {
+                    $match: {
+                        id : { $in : mediaIds }
+                    }
+                };
+
+                mediaAggregation.push(match);
+                
+                return Q(this.collection(config.get('databaseConfig:databases:core'), 'media')
+                    .aggregate(mediaAggregation).toArray())
+                    .then((media) => {
+                        const mediaObject = media.reduce((obj, item) => Object.assign(obj, { [item.id]: item }), {})
+                        
+                        const claimantsWithMedia =  claimants.results.map( rating => rating.media ? { ...rating, media: mediaObject[rating.media.oid]} : rating )
+                        return { ...claimants, results: claimantsWithMedia }
+                    });
+            })
             .then((result) => {
                 this.logger.info('Retrieved the results');
                 const response = {};
