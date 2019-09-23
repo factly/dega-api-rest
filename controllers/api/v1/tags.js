273204d732c37e9e1249e575a775e4f72e4fc576
Monark <monark@factly.in>
2019-09-23 12:49:59 +0530

    fixes #68

--- controllers/api/v1/tags.js
+++ controllers/api/v1/tags.js
@@ -22,6 +22,24 @@ function getTag(req, res, next) {
     }).catch(next);
 }
 
+function getTagBySlug(req, res, next) {
+    const logger = req.logger;
+    utils.setLogTokens(logger, 'tags', 'getTagBySlug', req.query.client, null);
+    const model = new TagModel(logger);
+    return model.getTagBySlug(
+        req.app.kraken, 
+        req.query.client, 
+        req.params.slug
+    ).then((result) => {
+        if (result) {
+            res.status(200).json(result);
+            return;
+        }
+        res.sendStatus(404);
+    }).catch(next);
+}
+
 module.exports = function routes(router) {
     router.get('/', getTag);
+    router.get('/:slug', getTagBySlug);
 };
--- models/tag.js
+++ models/tag.js
@@ -3,6 +3,20 @@ const Q = require('q');
 const MongoPaging = require('mongo-cursor-pagination');
 const utils = require('../lib/utils');
 
+const tagProject = {
+    $project : {
+        id: '$_id',
+        _id: 0,
+        class: '$_class',
+        name: 1,
+        slug: 1,
+        description: 1,
+        clientId: '$client_id',
+        createdDate: '$created_date',
+        lastUpdatedDate: '$last_updated_date'
+    }
+}
+
 class TagModel extends MongoBase {
     /**
      * Creates a new TagModel.
@@ -17,26 +31,14 @@ class TagModel extends MongoBase {
     getTag(config, clientId, sortBy, sortAsc, limit, next, previous) {
         const query = {};
         if (clientId) {
-            query.clientId = clientId;
+            query.client_id = clientId;
         }
 
         const match = { $match: query };
 
         const aggregations = [
-            {
-                $project : {
-                    id: '$_id',
-                    _id: 0,
-                    class: '$_class',
-                    name: 1,
-                    slug: 1,
-                    description: 1,
-                    clientId: '$client_id',
-                    createdDate: '$created_date',
-                    lastUpdatedDate: '$last_updated_date'
-                }
-            },
             match,
+            tagProject
         ];
 
         const pagingObj = utils.getPagingObject(aggregations, sortBy, sortAsc, limit, next, previous, true);
@@ -52,6 +54,37 @@ class TagModel extends MongoBase {
                 response.paging.previous = result.previous;
                 response.paging.hasPrevious = result.hasPrevious;
                 return response;
+            });
+    }
+
+    getTagBySlug(config, clientId, slug){
+        const query = {
+            slug: slug
+        };
+        
+        if (clientId) {
+            query.client_id = clientId;
+        }
+
+        const match = { $match: query };
+
+        const aggregations = [
+            match,
+            tagProject,
+        ];
+
+        const database = config.get('databaseConfig:databases:core');
+        return Q(this.collection(database)
+            .aggregate(aggregations).toArray())
+            .then((result) => {
+                this.logger.info('Retrieved the results');
+                
+                if(result && result.length === 1) 
+                    return {
+                        data: result[0]
+                    };
+
+                return
             });
     }
 }
